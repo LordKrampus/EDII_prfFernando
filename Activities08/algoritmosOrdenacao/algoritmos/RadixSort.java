18a7662578d882e05b4fb45651f6f996b386df40
Angel R Ferreira <52417632+LordKrampus@users.noreply.github.com>
2021-02-06 12:56:13 -0300

    Update RadixSort.java

--- Activities08/algoritmosOrdenacao/algoritmos/RadixSort.java
+++ Activities08/algoritmosOrdenacao/algoritmos/RadixSort.java
@@ -3,99 +3,56 @@ import java.util.Vector;
 
 //ainda não funciona
 public class RadixSort extends SortAlgorithm{
+    private int maiorElemento;
+    private int menorElemento;
+
     @Override
     public long realizar(Integer[] dados){
-        int maiorElemento = dados[0];
-        int menorElemento = 0;
-        int length;
+        this.maiorElemento = dados[0];
+        this.menorElemento = 0;
 
-        int i;
         // descobrir o maior elemento, e assim a maior quantidade de digitos
-        for(i = 0; i < dados.length; i++){
-            if(dados[i] > maiorElemento)
-                maiorElemento = dados[i];
-            else if(dados[i] < 0 && dados[i] < menorElemento)
-                menorElemento = dados[i];
+        for(int i = 0; i < dados.length; i++){
+            if(dados[i] > this.maiorElemento)
+                this.maiorElemento = dados[i];
+            else if(dados[i] < 0 && dados[i] < this.menorElemento)
+                this.menorElemento = dados[i];
         }
-        length = this.calcularLength(maiorElemento, menorElemento);
+        int maxQtdDigitos = String.valueOf(
+                (this.maiorElemento > -this.menorElemento?
+                        this.maiorElemento: this.menorElemento)).length();
 
-        //https://pt.wikipedia.org/wiki/Radix_sort
-        int maxDigit = String.join(String.valueOf(maiorElemento)).length();
-        for(int digit = 0; digit < maxDigit; digit ++){
-            int power = (int) Math.pow(10, digit+1);
-
-            int z[][] = new int[dados.length][10];
-            int n[] = new int[10];
-
-            for( i = 0; i < dados.length; i++){
-                int num = dados[i];
-                z[n[((num - menorElemento)%power)/(power/10)]][((num - menorElemento)%power)/(power/10)] = num;
-                n[((num - menorElemento)%power)/(power/10)]++;
-
-            }
-            int c = 0;
-            for( i = 0; i < 10; i++){
-                for(int j = 0; j < dados.length; j++){
-                    if(j < n[i]){
-                        dados[c] = z[j][i];
-                        c++;
-                    }else{break;}
-                }
-            }
-
-        }
+        this.ordenarLSD(dados, maxQtdDigitos);
 
         return super.iteracoes;
     }
 
-    /*
-    @Override
-    public long realizar(Integer[] dados){
-        int maiorElemento = dados[0];
-        int menorElemento = 0;
-        int length;
-
+    private void ordenarLSD(Integer[] dados, int maxQtdDigitos){
         int i;
-        // descobrir o maior elemento, e assim a maior quantidade de digitos
-        for(i = 0; i < dados.length; i++){
-            if(dados[i] > maiorElemento)
-                maiorElemento = dados[i];
-            else if(dados[i] < 0 && dados[i] < menorElemento)
-                menorElemento = dados[i];
-        }
-        length = this.calcularLength(maiorElemento, menorElemento);
 
-        int baseDigito = 10;
-        while(maiorElemento / baseDigito > 0){
-            //Vector<Vector<Integer>> distribuicao = new Vector<Vector<Integer>>(10);
+        int iteracao = 1; //contagem, para voltas por quantidade de digitos do maior elemento
+        while(iteracao <= maxQtdDigitos){
+            int potencia = (int) Math.pow(10, iteracao);
             Vector<Integer>[] aux = new Vector[10];
             for(i = 0; i < 10; i++){
-                aux[i] = new Vector<Integer>();
+                aux[i] = new Vector<Integer>(); //inciciando o vetor de vetor dinamico "Vector<Integer>"
             }
-            for (i = 0; i < dados.length; i++){
-                int indice = ((dados[i] - menorElemento) % baseDigito);
-                if (indice < 0){
-                    indice = 0;
-                }
-                //aux[(dados[i] + length) % baseDigito].add(dados[i]);
-                aux[indice].add(dados[i] - menorElemento);
+
+            // distribuindo por digito da potencia no aux (Vector)
+            for(i = 0; i < dados.length; i++){
+                aux[((dados[i] - this.menorElemento)%potencia)/(potencia/10)].add(dados[i]);
+                super.iteracoes++;
             }
+
+            // ordenando pela valor do digito na potencia
             int indice = 0;
-            for (i = 0; i < 10; i++){
-                int j = 0;
-                while(!aux[i].isEmpty())
-                    dados[indice++] = aux[i].elementAt(j++);
+            for(i = 0; i < 10; i++){
+                while(!aux[i].isEmpty()){
+                    dados[indice++] = aux[i].remove(0);
+                    super.iteracoes++;
+                }
             }
-
-            baseDigito *= 10;
+            iteracao++;
         }
-
-
-        return super.iteracoes;
-    }
-     */
-
-    private int calcularLength(int indiceA, int indiceB){
-        return (indiceA < 0? -indiceA + 1 : indiceA - indiceB + 1);
     }
 }
